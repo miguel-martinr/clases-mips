481799b479954022e17a8f6f72f4d2c6f77eaba0
Mordor1110 <45132495+Mordor1110@users.noreply.github.com>
2020-08-20 11:13:57 +0100

    p5-j: c++, MIPS msng

--- p5/javi/esqueleto.asm
+++ p5/javi/esqueleto.asm
@@ -3,8 +3,8 @@ cadena: 	.asciiz "Practica 5 de Principios de Computadores. Quedate en casa!!"
 cadena2:	.asciiz "roma tibi subito m otibus ibit amor"
 cadtiene:	.asciiz " tiene "
 cadcarac:	.asciiz " caracteres.\n"
-cadespal:	.asciiz "Es palíndroma.\n\n"
-cadnoespal:	.asciiz "No es palíndroma.\n\n"
+cadespal:	.asciiz "Es palindroma.\n\n"
+cadnoespal:	.asciiz "No es palindroma.\n\n"
 	.text
 
 strlen:  # numero de caracteres que tiene una cadena sin considerar el '\0'
@@ -12,26 +12,76 @@ strlen:  # numero de caracteres que tiene una cadena sin considerar el '\0'
 		 # $a0 tiene la direccion de la cadena
 		 # $v0 devuelve el numero de caracteres
 
-		 # INTRODUCE AQUÍ EL CÓDIGO DE LA FUNCIÓN strlen SIN CAMBIAR LOS ARGUMENTOS
+		 # INTRODUCE AQUI EL CODIGO DE LA FUNCION strlen SIN CAMBIAR LOS ARGUMENTOS
 		 
 reverse_i:  # funcion que da la vuelta a una cadena
 			# $a0 cadena a la que hay que dar la vuelta
 			# $a1 numero de caracternes que tiene la cadena
-			# $v0 1 Si es palíndroma 0 si no lo es
+			# $v0 1 Si es palindroma 0 si no lo es
 			
-			# INTRODUCE AQUÍ EL CÓDIGO DE LA FUNCIÓN reverse_i SIN CAMBIAR LOS ARGUMENTOS
-				
+			# INTRODUCE AQUI EL CODIGO DE LA FUNCION reverse_i SIN CAMBIAR LOS ARGUMENTOS
 
-reverse_r:  # funcion que da la vuelta a una cadena
+reverse_r:  
+      # funcion que da la vuelta a una cadena
 			# $a0 cadena a la que hay que dar la vuelta
 			# $a1 numero de caracternes que tiene la cadena
-			# $v0 1 Si es palíndroma 0 si no lo es
+			# $v0 1 Si es palindroma 0 si no lo es
 			
-			# INTRODUCE AQUÍ EL CÓDIGO DE LA FUNCIÓN reverse_r SIN CAMBIAR LOS ARGUMENTOS
-				
+			# INTRODUCE AQUI EL CODIGO DE LA FUNCION reverse_r SIN CAMBIAR LOS ARGUMENTOS
+  
+   #Prologo: guardamos en la pila las cosas que necesitamos
+   addi $sp,-4
+   sw $ra,0($sp)
+
+
+
+
+
+  reverse_r_end:
+
+   #Epilogo: deshacemos lo que hicimos en el prologo
+   lw $ra,0($sp)
+   addi $sp,4
+
+jr $ra
+
+
+
+
+
+
 
 main:
-			# INTRODUCE AQUÍ EL CÓDIGO DE LA FUNCIÓN main QUE REPRODUZCA LA SALIDA COMO EL GUIÓN
-			# INVOCANDO A LA FUNCIÓN strlen DESPUÉS DE CADA MODIFICACIÓN DE LAS CADENAS
+			# INTRODUCE AQUI EL CODIGO DE LA FUNCION main QUE REPRODUZCA LA SALIDA COMO EL GUION
+			# INVOCANDO A LA FUNCION strlen DESPUES DE CADA MODIFICACION DE LAS CADENAS
 			
-			
+  #Imprimir cadena
+  li $v0,4
+  la $a0,cadena
+  syscall
+
+  #Imprimir: cadtiene
+  li $v0,4
+  la $a0,cadtiene
+  syscall
+
+  #Llamar a strlen
+# la $a0,cadena
+# jal strlen
+
+# Ahora en $v0 deberia estar la longitud de: cadena
+
+ #Imprimimos la longitud
+ li $v0,1
+ move $a0,$v0
+ syscall
+
+ #Imprimimos la cadena: cadcaracteres
+ li $v0,4
+ la $a0,cadcarac
+ syscall
+
+
+
+  li $v0,10
+  syscall
---
+++ p5/javi/p5.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+
+int strlen(char* str_) {
+  char* str = str_;
+
+  int cont = 0;
+  while (*str != '\0') {
+    cont++;
+    str++;
+  }
+
+  if (*(--str) == '\n') {
+    *str = '\0';
+    --cont;
+  } 
+
+  return cont;
+}
+
+bool reverse_rr(char* str, int length, bool& es_pal) {
+
+  //char* str = str_;
+  if (length == 1 || length == 0)
+    return es_pal;
+
+  length--;
+
+  char aux = *str; 
+
+  *str = *(str + length);
+
+  if (aux != *str)
+    es_pal = false;
+
+  *(str + length) = aux;
+
+  str++;
+  length--;
+
+  reverse_rr(str, length, es_pal);
+
+}
+
+bool reverse_r(char* str, int length) {
+  bool es_pal = true;
+  return reverse_rr(str, length, es_pal);
+}
+
+
+// c a d e n a
+// 0 1 2 3 4 5
+
+//c a d d e n a
+//0 1 2 3 4 5 6
+
+//r o t o r r
+//0 1 2 3 4
+
+int main(void) {
+
+
+  char str[50];
+
+  cout << " > "; 
+  cin >> str;
+
+ bool pal = reverse_r(str, strlen(str));
+
+ cout << str << endl;
+ if (pal) {
+   cout << "es pal\n";
+ } else {
+   cout << "no es pal\n";
+ }
+
+
+
+  return 0;
+}
